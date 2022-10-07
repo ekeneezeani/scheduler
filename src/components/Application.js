e60712044dd425de5c49725fa43ece6a2b7eae24
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-10-06 22:49:15 -0500

    Feching days from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,54 +7,47 @@ import axios from "axios"
 
 
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 
 export default function Application(props) {
-
-  useEffect(()=>{
-    axios.get("http://localhost:8001/api/days")
-    .then((response) =>{
-      console.log(response)
-    })
-  },[])
 
   const [state, setState] = useState({
     day: "Monday",
@@ -62,27 +55,28 @@ export default function Application(props) {
     appointments: {}
   });
 
- 
+  const dailyAppointments = [];
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const setDay = (day) => {
+    setState(prev =>({...prev,day}))
+  }
+  
+  const setDays = (days)=>{
+    setState(prev => ({ ...prev, days }));
+  }
+  
+  useEffect(()=>{
+    axios.get("http://localhost:8001/api/days")
+    .then((response) =>{
+      setDays(response.data)
+      console.log(response.data)
+    })
+  },[])
 
-  const appointmentList = Object.values(appointments).map((appointment)=>{
+
+
+
+  const appointmentList = dailyAppointments.map((appointment)=>{
     return(
       <Appointment 
       key={appointment.id} 
@@ -91,9 +85,7 @@ export default function Application(props) {
     )
   })
 
-  return (
-
-    
+  return (    
     <main className="layout">
       <section className="sidebar">
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
